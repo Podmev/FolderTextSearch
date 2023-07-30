cb9f4ad23f5716579ebffd62c0c41d385f44a703
Dmitriy Pozdin <podmev@gmail.com>
2023-07-31 02:10:11 +0300

    .space.kts: remove sequential block

--- .space.kts
+++ .space.kts
@@ -12,43 +12,37 @@ job("Build and run tests") {
         outOfMemory { enabled = true }
     }
 
-    sequential {
-        container("openjdk:11") {
-            resources {
-                cpu = 512
-                memory = 2048
-            }
-            //run kotlin script
-            kotlinScript { api ->
-                println("Running in branch: " + api.gitBranch())
-
-                val path = "~/build"
-                val sharedFile = File(path)
-                api.fileShare().put(sharedFile, "artifact")
-
-                api.gradlew("build")
-                val recipient = MessageRecipient.Channel(ChatChannel.FromName("CI-Channel"))
-                val content = ChatMessage.Text("Build has completed - build number: " + api.executionNumber())
-                api.space().chats.message.sendMessage(recipient, content)
-            }
-
+    container("openjdk:11") {
+        resources {
+            cpu = 512
+            memory = 2048
         }
+        kotlinScript { api ->
+            println("Running in branch: " + api.gitBranch())
 
-        container("openjdk:11") {
-            resources {
-                cpu = 512
-                memory = 2048
-            }
-            //run kotlin script
-            kotlinScript { api ->
-                println("Running in branch: " + api.gitBranch())
-                api.gradlew("test")
-                val recipient = MessageRecipient.Channel(ChatChannel.FromName("CI-Channel"))
-                val content = ChatMessage.Text("Test has completed - build number: " + api.executionNumber())
-                api.space().chats.message.sendMessage(recipient, content)
-            }
+            val path = "~/build"
+            val sharedFile = File(path)
+            api.fileShare().put(sharedFile, "artifact")
 
+            api.gradlew("build")
+            val recipient = MessageRecipient.Channel(ChatChannel.FromName("CI-Channel"))
+            val content = ChatMessage.Text("Build has completed - build number: " + api.executionNumber())
+            api.space().chats.messages.sendMessage(recipient, content)
         }
-
     }
+
+    container("openjdk:11") {
+        resources {
+            cpu = 512
+            memory = 2048
+        }
+        kotlinScript { api ->
+            println("Running in branch: " + api.gitBranch())
+            api.gradlew("test")
+            val recipient = MessageRecipient.Channel(ChatChannel.FromName("CI-Channel"))
+            val content = ChatMessage.Text("Test has completed - build number: " + api.executionNumber())
+            api.space().chats.messages.sendMessage(recipient, content)
+        }
+    }
+
 }
