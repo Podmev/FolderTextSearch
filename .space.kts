2233fda24faa3825817397e40dc9d6cb251c0212
Dmitriy Pozdin <podmev@gmail.com>
2023-07-31 01:53:58 +0300

    init .space.kts file with build and test

---
+++ .space.kts
@@ -0,0 +1,52 @@
+job("Build and run tests") {
+    startOn {
+        gitPush { enabled = true }
+        //schedule { "0 8 * * *"} every day at 8 am
+    }
+
+    failOn {
+        nonZeroExitCode { enabled = true }
+        testFailed { enabled = true }
+        outOfMemory { enabled = true }
+    }
+
+    sequencial {
+        container("openjdk:11") {
+            resources {
+                cpu = 512
+                memory = 2048
+            }
+            //run kotlin script
+            kotlinScript { api ->
+                println("Running in branch: " + api.gitBranch())
+
+                val path = "~/build"
+                val sharedFile = File(path)
+                api.fileShare().put(sharedFile, "artifact")
+
+                api.gradlew("build")
+                val recipient = MessageRecipient.Channel(ChatChannel.FromName("CI-Channel"))
+                val content = ChatMessage.Text("Build has completed - build number: " + api.executionNumber)
+                api.space().chats.message.sendMessage(recipient, content)
+            }
+
+        }
+
+        container("openjdk:11") {
+            resources {
+                cpu = 512
+                memory = 2048
+            }
+            //run kotlin script
+            kotlinScript { api ->
+                println("Running in branch: " + api.gitBranch())
+                api.gradlew("test")
+                val recipient = MessageRecipient.Channel(ChatChannel.FromName("CI-Channel"))
+                val content = ChatMessage.Text("Test has completed - build number: " + api.executionNumber)
+                api.space().chats.message.sendMessage(recipient, content)
+            }
+
+        }
+
+    }
+}
