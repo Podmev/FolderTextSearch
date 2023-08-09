385aef0fc06fb8cc2dd7fbe2d3ce2a6610c06844
Dmitriy Pozdin <podmev@gmail.com>
2023-08-09 05:50:01 +0300

    reformatting String.indicesOf

--- src/main/kotlin/utils/string.kt
+++ src/main/kotlin/utils/string.kt
@@ -1,11 +1,12 @@
 package utils
 
-/*Finds all indices of starts of token in line.
-* Works lazily.
-* For empty token returns empty sequence
-* */
+/**
+ * Finds all indices of starts of token in line.
+ * Works lazily.
+ * For empty token returns empty sequence
+ * */
 fun String.indicesOf(token: String, ignoreCase: Boolean = false): Sequence<Int> {
-    if(token.isEmpty()){
+    if (token.isEmpty()) {
         return emptySequence()
     }
     fun next(startOffset: Int) = this.indexOf(token, startOffset, ignoreCase).takeIf { it != -1 }
