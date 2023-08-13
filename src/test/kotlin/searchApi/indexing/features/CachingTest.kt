48ae53d78e7e4eef9fba780f0ff807d020edd64e
Dmitriy Pozdin <dmitriy.pozdin@bia-tech.ru>
2023-08-14 01:05:51 +0300

    finished CachingTest for indexing

--- src/test/kotlin/searchApi/indexing/features/CachingTest.kt
+++ src/test/kotlin/searchApi/indexing/features/CachingTest.kt
@@ -1,3 +1,35 @@
 package searchApi.indexing.features
-//TODO tests
-class CachingTest
+
+import api.SearchApi
+import api.tools.searchapi.syncPerformIndex
+import impl.trigram.TrigramSearchApi
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import searchApi.common.commonSetup
+
+/**
+ * Checking how cache works on indexing requests.
+ * Tests show that there is cache.
+ * */
+class CachingTest {
+    /**
+     * Generator of SearchApi, so every time we use it, it is with fresh state.
+     * */
+    private val searchApiGenerator: () -> SearchApi = { TrigramSearchApi() }
+
+    /**
+     * Comparing time for first and second indexing the same folder.
+     * Second almost doesn't take any time - uses cache.
+     * */
+    @Test
+    fun repeatIndexHasCacheTest() {
+        val folder = commonSetup.srcFolder
+        val searchApi = searchApiGenerator()
+        val state1 = searchApi.syncPerformIndex(folder)
+        val state2 = searchApi.syncPerformIndex(folder)
+        Assertions.assertAll(
+            { Assertions.assertTrue(state1.totalTime > 500, "At first there is no cache, so index works longer") },
+            { Assertions.assertTrue(state2.totalTime < 5, "Repeat index works fast - it uses cache") }
+        )
+    }
+}
