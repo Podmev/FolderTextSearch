2d236ab55514ee5288a13cd13ec7b5ed77cfe7c9
Dmitriy Pozdin <podmev@gmail.com>
2023-08-21 02:15:26 +0300

    add comment to entry point of big tests

--- src/test/kotlin/searchApi/bigTest/main.kt
+++ src/test/kotlin/searchApi/bigTest/main.kt
@@ -1,11 +1,24 @@
 package searchApi.bigTest
 
+/**
+ * Entry point to test big tests in CurProjectTrigramTest and IntellijIdeaTrigramTest
+ *
+ *
+ * CurProjectTrigramTest:
+ * ```
+ * CurProjectTrigramTest().justIndex()
+ * CurProjectTrigramTest().indexWithSearchOneToken()
+ * CurProjectTrigramTest().searchOneTokenAfterIndex()
+ * ```
+ * IntellijIdeaTrigramTest:
+ * ```
+ * IntellijIdeaTrigramTest().justIndex()
+ * IntellijIdeaTrigramTest().justIndexWithCancel()
+ * IntellijIdeaTrigramTest().indexWithSearchOneToken()
+ * IntellijIdeaTrigramTest().indexWithSearchManyTokens()
+ * IntellijIdeaTrigramTest().searchOneTokenAfterIndex()
+ * ```
+ * */
 fun main() {
-//    CurProjectTrigramTest().justIndex()
-//    CurProjectTrigramTest().searchOneTokenAfterIndex()
     IntellijIdeaTrigramTest().searchOneTokenAfterIndex()
-//    IntellijIdeaTrigramTest().searchOneTokenAfterIndexWithSimpleTrigramMap()
-//    IntellijIdeaTrigramTest().justIndex()
-//    IntellijIdeaTrigramTest().indexWithSearchManyTokens()
-//    IntellijIdeaTrigramTest().justIndexWithCancel()
 }
